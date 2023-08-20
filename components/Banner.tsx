2453789bd0d295969300a4dcd39f7e83add3f9d5
HUBAGBB <93278401+HUBAGBB@users.noreply.github.com>
2023-08-20 19:39:44 +0900

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -23,7 +23,7 @@ interface Props {
       <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
         <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
           <Image
-            src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path || movie?.original_language}`}
+            src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
             layout="fill"
             objectFit="cover"
           />
