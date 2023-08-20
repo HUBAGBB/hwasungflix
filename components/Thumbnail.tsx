e24ea8b5fd7d2e1ebb14f08b341c19b00ca4e221
HUBAGBB <93278401+HUBAGBB@users.noreply.github.com>
2023-08-20 19:42:28 +0900

    Update Thumbnail.tsx

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -17,6 +17,7 @@ function Thumbnail({ movie }: Props) {
         }`}
         className="rounded-sm object-cover md:rounded"
         layout="fill"
+        alt="Picture of the author"
       />
     </div>
   )
